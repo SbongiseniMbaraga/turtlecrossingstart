26214e9b1fcc758be5995cfd0648dc5d430992cf
SbongiseniMbaraga <sbongiseni2@hotmail.com>
2021-02-13 15:00:58 +0200

    Initial commit

---
+++ car_manager.py
@@ -0,0 +1,34 @@
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager:
+
+    def __init__(self):
+        super().__init__()
+        self.all_cars = []
+        self.car_design_position_and_new_car()
+        self.move_speed = STARTING_MOVE_DISTANCE
+
+    def car_design_position_and_new_car(self):
+        random_chance = random.randint(1, 7)
+        if random_chance == 1:
+            new_car = Turtle()
+            new_car.shape("square")
+            new_car.penup()
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.color(random.choice(COLORS))
+            new_car.setheading(180)
+            new_car.goto(x=280, y=random.randint(-290, 290))
+            self.all_cars.append(new_car)
+
+    def car_movement(self):
+        for car in self.all_cars:
+            car.forward(self.move_speed)
+
+    def car_movement_speed(self):
+        self.move_speed += MOVE_INCREMENT
---
+++ main.py
@@ -0,0 +1,40 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+# screem object
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+# player moves forward and bind key to method
+player = Player()
+screen.listen()
+screen.onkey(player.move_forward, "Up")
+
+# cars are generated
+cars = CarManager()
+
+# score
+scoreboard = Scoreboard()
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    cars.car_design_position_and_new_car()
+    cars.car_movement()
+    # when player is clear, give point and reset position
+    if player.ycor() == 300:
+        scoreboard.add_score()
+        player.reset_position()
+        cars.car_movement_speed()
+    # if the player touches one of the cars then game over
+    for car in cars.all_cars:
+        if player.distance(car) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+
+screen.exitonclick()
